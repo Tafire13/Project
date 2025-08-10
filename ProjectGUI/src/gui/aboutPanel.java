944a7c4fa5a96fa3b959b06d53fa5c34e25ea341
Tafire13 <67011212063@msu.ac.th>
2025-08-10 20:28:34 +0700

    update

--- ProjectGUI/src/gui/aboutPanel.java
+++ ProjectGUI/src/gui/aboutPanel.java
@@ -6,42 +6,43 @@ import manage.constant;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 public class aboutPanel {
     constant con = new constant();
     ThemeColors themeColors = new ThemeColors();
+
     private JPanel topPanel;
     private JPanel buttomPanel;
     private JPanel centerPanel;
-    private JPanel[] imagePanel = new JPanel[3];
+    private JPanel[] imagePanel;
     private ImageIcon[] imageIcon;
     private JLabel[] imageLabel = new JLabel[3];
     private JLabel[] DataLabel = new JLabel[3];
+    private JButton backButton;
+
     public aboutPanel() {
-        topPanel = new JPanel();
-        centerPanel = new JPanel();
-        buttomPanel = new JPanel();
-        imagePanel = new JPanel[3];
-        imageIcon = new ImageIcon[3];
-        setTopPanel(topPanel);
-        setBottomLabel(buttomPanel);
-        setCenterPanel(centerPanel);
-        setImagePanel(imagePanel);
+        setTopPanel();
+        setBottomLabel();
+        setCenterPanel();
+        setImagePanel();
         setImageIcon();
-        setDataLabel(DataLabel);
+        setDataLabel();
+        setBackButton();
+
         addPanelImage(centerPanel);
         addImage(imageIcon, imagePanel);
         addData(DataLabel, imagePanel);
+
+        buttomPanel.add(backButton);
+
         JLabel title = new JLabel("About Group");
         title.setFont(new Font("Arial", Font.BOLD, 48));
         title.setBounds(500, 30, 2000, 50);
         topPanel.add(title, BorderLayout.CENTER);
     }
 
-    public void setTopPanel(JPanel topPanel) {
-        this.topPanel = topPanel;
+    public void setTopPanel() {
+        this.topPanel = new JPanel();
         this.topPanel.setPreferredSize(new Dimension(100, 100));
         this.topPanel.setBackground(themeColors.white);
         this.topPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 20));
@@ -51,20 +52,20 @@ public class aboutPanel {
         return topPanel;
     }
 
-    public void setBottomLabel(JPanel buttomPanel) {
-        this.buttomPanel = buttomPanel;
+    public void setBottomLabel() {
+        this.buttomPanel = new JPanel();
         this.buttomPanel.setPreferredSize(new Dimension(100, 100));
         this.buttomPanel.setBorder(new LineBorder(themeColors.black, 2));
         this.buttomPanel.setBackground(themeColors.white);
-        this.buttomPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 50, 40));
+        this.buttomPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 50, 25));
     }
 
     public JPanel getButtomPanel() {
         return buttomPanel;
     }
 
-    public void setCenterPanel(JPanel centerPanel) {
-        this.centerPanel = centerPanel;
+    public void setCenterPanel() {
+        this.centerPanel = new JPanel();
         this.centerPanel.setBorder(new LineBorder(themeColors.black, 2));
         this.centerPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 25));
     }
@@ -73,13 +74,13 @@ public class aboutPanel {
         return centerPanel;
     }
 
-    public void setImagePanel(JPanel[] imagePanel) {
-        this.imagePanel = imagePanel;
-        for (int i = 0; i < imagePanel.length; i++) {
+    public void setImagePanel() {
+        this.imagePanel = new JPanel[3];
+        for (int i = 0; i < 3; i++) {
             this.imagePanel[i] = new JPanel();
             this.imagePanel[i].setPreferredSize(new Dimension(300, 500));
             this.imagePanel[i].setBorder(new LineBorder(themeColors.black, 2));
-            this.imagePanel[i].setLayout(new FlowLayout(FlowLayout.CENTER,0 , 0));
+            this.imagePanel[i].setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
             this.imagePanel[i].setBackground(themeColors.white);
         }
     }
@@ -87,24 +88,20 @@ public class aboutPanel {
     public void addPanelImage(JPanel panel) {
         for (int i = 0; i < imagePanel.length; i++) {
             panel.add(imagePanel[i]);
+        }
+    }
 
-        }
-    }
-    public void addData(JLabel[] labels, JPanel[] panel){
-        for (int i = 0; i < labels.length; i++) {
-            panel[i].add(labels[i]);
-        }
-    }
     public void setImageIcon() {
-        this.imageIcon = new ImageIcon[]{
-                loadImage("/image/chok.JPG", 295, 400),
-                loadImage("/image/khunQ.JPG", 295, 400),
-                loadImage("/image/p.JPG", 295, 400)
-        };
+        this.imageIcon = new ImageIcon[3];
+        this.imageIcon[0] = loadImage("/image/chok.JPG", 295, 400);
+        this.imageIcon[1] = loadImage("/image/khunQ.JPG", 295, 400);
+        this.imageIcon[2] = loadImage("/image/p.JPG", 295, 400);
     }
+
     private ImageIcon loadImage(String path, int width, int height) {
         java.net.URL imgURL = getClass().getResource(path);
         if (imgURL == null) {
+            System.err.println("Image not found: " + path);
             return null;
         }
         Image img = new ImageIcon(imgURL).getImage()
@@ -115,14 +112,32 @@ public class aboutPanel {
     public void addImage(ImageIcon[] imageIcon, JPanel[] panel) {
         for (int i = 0; i < imageIcon.length; i++) {
             imageLabel[i] = new JLabel(imageIcon[i]);
-            panel[i].add(imageLabel[i], BorderLayout.CENTER);
+            panel[i].add(imageLabel[i]);
         }
     }
 
-    public void setDataLabel(JLabel[] dataLabel) {
-        for (int i = 0; i < dataLabel.length; i++) {
-            dataLabel[i] = new JLabel("<html>Name :"+con.name[i] + "<br> SID :" + con.SID[i] + "</html>");
-            dataLabel[i].setFont(new Font("TH Sarabun New", Font.PLAIN, 32));
+    public void setDataLabel() {
+        this.DataLabel = new JLabel[3];
+        for (int i = 0; i < DataLabel.length; i++) {
+            DataLabel[i] = new JLabel("<html>Name : " + con.name[i] + "<br> SID : " + con.SID[i] + "</html>");
+            DataLabel[i].setFont(new Font("TH Sarabun New", Font.PLAIN, 32));
         }
+    }
+
+    public void addData(JLabel[] labels, JPanel[] panel) {
+        for (int i = 0; i < labels.length; i++) {
+            panel[i].add(labels[i]);
+        }
+    }
+
+    public void setBackButton() {
+        this.backButton = new JButton("Back");
+        backButton.setPreferredSize(new Dimension(100, 50));
+        backButton.setFocusable(false);
+        backButton.setBackground(themeColors.red);
+    }
+
+    public JButton getBackButton() {
+        return backButton;
     }
 }
